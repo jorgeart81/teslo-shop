09a75deb9e91eb07a7247fa483ebc96030c1ace9
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-30 17:21:56 -0500

    Create useUIStore

---
+++ src/store/ui/ui.store.ts
@@ -0,0 +1,28 @@
+import { create, StateCreator } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+interface UIStore {
+  isSideMenuOpen: boolean;
+}
+
+interface Actions {
+  closeSideMenu: () => void;
+  openSideMenu: () => void;
+}
+
+const storeApi: StateCreator<
+  UIStore & Actions,
+  [['zustand/devtools', never]]
+> = set => ({
+  isSideMenuOpen: false,
+
+  // Actions
+  closeSideMenu: () => {
+    set({ isSideMenuOpen: false });
+  },
+  openSideMenu: () => {
+    set({ isSideMenuOpen: true });
+  },
+});
+
+export const useUIStore = create<UIStore & Actions>()(devtools(storeApi));
