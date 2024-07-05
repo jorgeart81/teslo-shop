aa8bcda8436e7ee505ab05731fe73fc09a3d7e11
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-04 19:09:28 -0500

    improve: Title component

--- src/components/ui/title/Title.tsx
+++ src/components/ui/title/Title.tsx
@@ -1,20 +1,23 @@
 import { titleFont } from '@/config/fonts';
+import clsx from 'clsx';
 import React from 'react';
 
 interface Props {
-  title: string;
-  subtitle?: string;
-  className?: string;
+	title: string;
+	subtitle?: string;
+	className?: string;
 }
 
 export const Title = ({ title, subtitle, className }: Props) => {
-  return (
-    <div className={`${className}`}>
-      <h1
-        className={`${titleFont.className} antialiased text-4xl font-semibold mt-10`}>
-        {title}
-      </h1>
-      {subtitle && <h3 className='mb-5 text-xl font-semibold uppercase mt-10'>{subtitle}</h3>}
-    </div>
-  );
+	return (
+		<div className={`${className}`}>
+			<h1
+				className={clsx(`${titleFont.className} antialiased text-4xl font-semibold mt-10`, {
+					'mb-5': !subtitle,
+				})}>
+				{title}
+			</h1>
+			{subtitle && <h3 className='mb-5 text-xl font-semibold uppercase mt-10'>{subtitle}</h3>}
+		</div>
+	);
 };
