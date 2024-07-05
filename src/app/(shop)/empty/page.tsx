bd4f28ede49b4896796ff5305e7c2b4514e11a8e
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-04 20:14:56 -0500

    Implement EmptyPage

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+import { IoCartOutline } from 'react-icons/io5';
+
 export default function EmptyPage() {
-  return <div>EmptyPage</div>;
+	return (
+		<div className='flex justify-center items-center h-[calc(100dvh-3.5rem)]'>
+			<IoCartOutline size={80} className='mx-5' />
+
+			<div className='flex flex-col items-center'>
+				<h1 className='text-xl font-semibold'>Tu carrito está vacio</h1>
+				<Link href='/' className='text-blue-500 mt-2 text-4xl'>
+					Regresar
+				</Link>
+			</div>
+		</div>
+	);
 }
