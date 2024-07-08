b50c622654dc64eceb6014c78daaf29c24a098b4
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-08 18:42:23 -0500

    refactor: index actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,2 +1,2 @@
-export * from './product/get-product-by-slug';
-export * from './product/product-pagination';
+export { getProductBySlug } from './product/get-product-by-slug';
+export { getPaginatedProductsWithImages } from './product/product-pagination';
