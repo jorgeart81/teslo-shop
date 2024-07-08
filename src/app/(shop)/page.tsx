cc0473b59e1068c4b1417148a149afdb0348ebbe
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-07 20:28:06 -0500

    Create Pagination.tsx

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,14 +1,27 @@
-import { Title } from '@/components/ui';
-import { initialData } from '@/seed/seed';
+import { getPaginatedProductsWithImages } from '@/actions';
 import { ProductGrid } from '@/components/products';
+import { Pagination, Title } from '@/components/ui';
+import { redirect } from 'next/navigation';
 
-const products = initialData.products;
+interface Props {
+	searchParams: {
+		page?: string;
+	};
+}
 
-export default function Home() {
-  return (
-    <>
-      <Title title='Tienda' subtitle='Todos los productos' />
-      <ProductGrid products={products} />
-    </>
-  );
+export default async function Home({ searchParams }: Props) {
+	const page = searchParams.page ? parseInt(searchParams.page) : 1;
+	const { products, currentPage, totalPages } = await getPaginatedProductsWithImages({ page });
+
+	if (products.length === 0 || currentPage > totalPages) {
+		redirect('/');
+	}
+
+	return (
+		<>
+			<Title title='Tienda' subtitle='Todos los productos' />
+			<ProductGrid products={products} />
+			<Pagination />
+		</>
+	);
 }
