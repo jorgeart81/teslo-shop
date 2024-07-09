eedf3fb320c26f8ac76f2264af68793ef664b9e1
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-09 15:55:05 -0500

    Implement next generateMetadata in ProductBySlugPage

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 export const revalidate = 604800; // 7 days
 
+import { Metadata, ResolvingMetadata } from 'next';
 import { notFound } from 'next/navigation';
 
 import { getProductBySlug } from '@/actions';
@@ -15,6 +16,24 @@ import { titleFont } from '@/config/fonts';
 interface Props {
 	params: {
 		slug: string;
+	};
+}
+
+export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
+	const slug = params.slug;
+	const product = await getProductBySlug(slug);
+
+	// optionally access and extend (rather than replace) parent metadata
+	// const previousImages = (await parent).openGraph?.images || [];
+	return {
+		title: product?.title ?? 'Producto no encontrado',
+		description: product?.description ?? '',
+		openGraph: {
+			title: product?.title ?? 'Producto no encontrado',
+			description: product?.description ?? '',
+			// images: ['/some-specific-page-image.jpg', ...previousImages],
+			images: [`/products/${product?.images[1]}`],
+		},
 	};
 }
 
