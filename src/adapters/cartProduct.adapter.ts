cf54b635c820e71b3d21116bd20658ecd3aff86b
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-10 12:56:59 -0500

    Create cartProductAdapter

---
+++ src/adapters/cartProduct.adapter.ts
@@ -0,0 +1,13 @@
+import type { CartProduct, Product } from '@/interfaces';
+
+export const cartProductAdapter = (product: any): CartProduct => {
+	return {
+		id: product.id,
+		slug: product.slug,
+		title: product.title,
+		price: product.price,
+		quantity: product.quantity,
+		size: product.size,
+		image: product.images[1],
+	};
+};
