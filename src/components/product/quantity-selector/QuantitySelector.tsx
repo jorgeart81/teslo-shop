fe498f5c1434f9d75996b3398ad0717bf3e09426
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-09 21:30:53 -0500

    Improve QuatitySelector

--- src/components/product/quantity-selector/QuantitySelector.tsx
+++ src/components/product/quantity-selector/QuantitySelector.tsx
@@ -1,26 +1,24 @@
-'use client';
-
-import { useState } from 'react';
 import { IoAddCircleOutline, IoRemoveCircleOutline } from 'react-icons/io5';
 
 interface Props {
+	limit?: number;
 	quantity: number;
+	onQuantityChanged: (value: number) => void;
 }
 
-export const QuantitySelector = ({ quantity }: Props) => {
-	const [count, setCount] = useState(0);
-	const onQuatityChange = (value: number) => {
-		if (count + value < 1) return;
-		setCount((prev) => prev + value);
+export const QuantitySelector = ({ limit = 10, quantity, onQuantityChanged }: Props) => {
+	const onValueChanged = (value: number) => {
+		if (quantity + value < 1 || quantity + value > limit) return;
+		onQuantityChanged(quantity + value);
 	};
 
-  return (
+	return (
 		<div className='flex items-center'>
-			<button onClick={() => onQuatityChange(-1)}>
+			<button disabled={quantity < 2} onClick={() => onValueChanged(-1)} className='disabled:opacity-50'>
 				<IoRemoveCircleOutline size={30} />
 			</button>
-			<span className='w-16 md:w-20 mx-3 px-5 py-1 bg-gray-100 text-center rounded'>{count}</span>
-			<button onClick={() => onQuatityChange(1)}>
+			<span className='w-16 md:w-20 mx-3 px-5 py-1 bg-gray-100 text-center rounded'>{quantity}</span>
+			<button disabled={quantity === limit} onClick={() => onValueChanged(1)} className='disabled:opacity-50'>
 				<IoAddCircleOutline size={30} />
 			</button>
 		</div>
