4bb14578d1151c124b53fc6f94f933e347df4d02
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-07-12 19:36:46 -0500

    Create authenticate action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,2 +1,4 @@
+export { authenticate } from './auth/login';
+
 export { getProductBySlug } from './product/get-product-by-slug';
 export { getPaginatedProductsWithImages } from './product/product-pagination';
